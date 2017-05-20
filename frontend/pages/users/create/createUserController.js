55dc9d675db9f22d879fdfa7e6e0e426bd53ea56
Diana Speranda <dsperanda@swingint.hr>
2017-05-20 19:33:30 +0200

    create user

--- frontend/pages/users/create/createUserController.js
+++ frontend/pages/users/create/createUserController.js
@@ -1,1 +1,38 @@
-hgss.controller("createUserController", function () {});
+hgss.controller("createUserController", ["$scope", "$state", "httpService", function ($scope, $state, httpService) {
+	
+	$scope.user = {
+		name: "",
+		phoneNumber: "",
+		adress: "",
+		job: "",
+		availbility: "",
+		station: "",
+		group: "",
+		membership: "",
+
+	}
+
+	$scope.value = {
+		name: "",
+		phoneNumber: "",
+		adress: "",
+		job: "",
+		availbility: "",
+		station: "",
+		group: "",
+		membership: "",
+	};
+
+	$scope.save = function()
+	{
+		$scope.value = $scope.user;
+		httpService.createNewUser($scope.user).then(response => {
+			$state.go("app.users")
+		});
+	}
+
+	$scope.back = function()
+	{
+		$state.go("app.users");
+	}
+}]);
