5284d99b76ae1d99808eea4ed1d8e90db50173d7
imedic <medic.ivan.hr@gmail.com>
2017-05-20 20:16:14 +0200

    User grid and search added

--- frontend/pages/users/usersController.js
+++ frontend/pages/users/usersController.js
@@ -3,9 +3,16 @@ hgss.controller("usersController", ["$scope", "$state", "httpService", function 
         $state.go('app.add-user');
     }
 
-    httpService.getAllUsers().then(function(res){
-    	console.log(res);
+    httpService.getAllUsers().then(function(response) {
+        $scope.users = response.data;
+        console.log(response.data);
     })
 
+    $scope.isCardView;
 
+    $scope.setCardView = function(value) {
+        $scope.isCardView = value;
+    }
+
+    $scope.searchQuery = "";
 }]);
