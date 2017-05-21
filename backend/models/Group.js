66b1672e1e22a152e0caf55af7d4462f122d49ed
imedic <medic.ivan.hr@gmail.com>
2017-05-21 10:15:01 +0200

    Quick last fix

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var GroupSchema = new Schema({
     name: String,
-    participants: Array
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 },{ timestamps: true });
 
 module.exports = mongoose.model('Group', GroupSchema);
